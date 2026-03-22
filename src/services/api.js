4bc827a0219ad56fade916c62d0466caf712431e
Gangatharan M <gangatharan8504@gmail.com>
2026-03-22 17:34:04 +0530

    Update API base URL to production endpoint

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:8081/api",
+  baseURL: "https://e-waste-backend-tlqa.onrender.com/api",
 });
 
 // Attach JWT automatically
